2e76ffb78200c5fa2bd526fbf38901405e4d4eca
Charles4982 <Charles4982@startravel.com.tw>
2019-12-26 23:43:31 +0800

    實作介面 定義 IConvert 介面，設計一個名稱 CLen 類別來實作 IConvert 介面的規格。

---
+++ slnInterface/slnInterface/CLen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slnInterface
+{
+    class CLen:IConvert
+    {
+        private double mult = 2.54;     //linch=2.54 cm
+        public double Mult              //實作倍率屬性
+        {
+            get { return mult; }
+            set { mult = value; }
+        }
+
+        /// <summary>
+        /// 實作 Convert()方法
+        /// </summary>
+        /// <param name="inch"></param>
+        public void Convert(double inch)
+        {
+            double cm = inch * Mult;
+            Console.WriteLine("英制單位 {0}吋 =公制單位 {1}公分",inch,cm.ToString("#.##"));
+        }
+    }
+}
---
+++ slnInterface/slnInterface/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slnInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入英制長度(吋): ");
+            double len = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            CLen lenght = new CLen();      //IConvert lenght=new CLen();
+            lenght.Convert(len);
+            Console.Read();
+        }
+    }
+}
