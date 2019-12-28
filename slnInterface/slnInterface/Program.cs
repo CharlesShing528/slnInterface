c1b42928d60092d766c464a607a8f94824ff9230
Charles4982 <Charles4982@startravel.com.tw>
2019-12-28 13:59:14 +0800

    實作介面 定義 IConvert 介面，設計一個名稱 CLen 類別來實作 IConvert 介面的規格。

--- slnInterface/slnInterface/Program.cs
+++ slnInterface/slnInterface/Program.cs
@@ -13,7 +13,7 @@ namespace slnInterface
             Console.WriteLine("輸入英制長度(吋): ");
             double len = float.Parse(Console.ReadLine());
             Console.WriteLine();
-            CLen lenght = new CLen();      //IConvert lenght=new CLen();
+            CLen lenght = new CLen();      //本行亦可改寫成 IConvert lenght=new CLen();
             lenght.Convert(len);
             Console.Read();
         }
