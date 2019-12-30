50760c0f31aee0a37150cd61d3ea5b220dff5b56
Charles4982 <Charles4982@startravel.com.tw>
2019-12-31 00:10:56 +0800

    interface應用在方法的實作

---
+++ slnInterface/Ex_interface_04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 一個實作應用在兩個 interface 上
+/// 如果類別實作了兩個介面包含相同名稱之方法，
+/// 則在類別上實作該方法會導致兩個介面都將該方法當做實作(Implementation) 使用。
+/// </summary>
+namespace Ex_interface_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass sc = new SampleClass();
+            IControl ctrl = (IControl)sc;
+            Isurface srfc = (Isurface)sc;
+
+            sc.Paint();
+            ctrl.Paint();
+            srfc.Paint();
+            Console.Read();
+        }
+    }
+
+    interface IControl
+    {
+        void Paint();
+    }
+    interface Isurface
+    {
+        void Paint();
+    }
+    class SampleClass:IControl,Isurface
+    {
+        public void Paint()
+        {
+            Console.WriteLine("Paint method in SampleClass");
+        }
+    }
+}
